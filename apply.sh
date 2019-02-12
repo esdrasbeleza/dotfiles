b0e154c318367dc766732c36666a375e5496749e
Esdras Beleza de Noronha <github@contato.esdrasbeleza.com>
2019-02-12 17:43:23 +0000

    Apply tmux

--- apply.sh
+++ apply.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-if [ ! -d "$HOME/.oh-my-zsh"]; then
+DOTFILES=`pwd`
+
+cd $HOME
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	cat zshrc > ~/.zshrc
+	cat "$DOTFILES/zshrc" > ~/.zshrc
 fi
 
+if [ ! -d "$HOME/.tmux" ]; then
+	git clone https://github.com/gpakosz/.tmux.git
+	ln -s -f .tmux/.tmux.conf
+	cat "$DOTFILES/tmux.conf.local" > ~/.tmux.conf.local
+fi
